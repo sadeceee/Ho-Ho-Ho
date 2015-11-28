f6969c856e09800c6652d123cfba58b68b159fa4
Dino Buskulic <Love@Dinos-MacBook-Pro.local>
2015-11-28 13:20:40 +0100

    Extended model for wishlist items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128111531) do
+ActiveRecord::Schema.define(version: 20151128121733) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      null: false
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20151128111531) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+
+  create_table "wishlist_items", force: :cascade do |t|
+    t.string   "Title"
+    t.float    "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "wishlists", force: :cascade do |t|
     t.integer  "user_id"

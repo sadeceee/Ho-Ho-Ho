1d0d1c98422e94977f43ba60a4c146eae13221ef
bDino <buskulic.dino@gmail.com>
2015-11-28 18:16:35 +0100

    Wishlist sum implemented

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,9 @@
 class Wishlist < ActiveRecord::Base
   validates :title, presence: true
   has_many :wishlist_items
+
+  def get_wishlist_sum
+    wishlist_items.reduce(0) { |sum,witem| sum + witem.price}
+  end
+
 end

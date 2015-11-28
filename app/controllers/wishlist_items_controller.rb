48e58b74853f39d9cba8b3a37174a3470de003ac
Michael Hanisch <Michael.hanisch@haw-hamburg.de>
2015-11-28 15:04:16 +0100

    done some ruby magic

---
+++ app/controllers/wishlist_items_controller.rb
@@ -0,0 +1,39 @@
+class WishlistItemsController < ApplicationController
+  # GET /wishlists
+  def index
+    @wishlist_item = WishlistItems.all
+  end
+
+  # GET /wishlists/1
+  def show
+      @wishlist_item = WishlistItems.find(params[:id])
+  end
+
+  # GET /wishlists/new
+  def new
+    @wishlist_item = WishlistItem.new
+  end
+
+  # POST /wishlists
+  def create
+    @wishlist_item = WishlistItem.new(wishlist_item_params)
+
+    if @wishlist_item.save
+      redirect_to @wishlist_item, notice: 'Item was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+
+  # DELETE /wishlists/1
+  def destroy
+    @wishlist_item.destroy
+    redirect_to wishlists_url, notice: 'Wishlist Item was successfully destroyed.'
+  end
+private
+  def wishlist_item_params
+    params.require(:wishlist_item).permit(:title, :price)
+  end
+end
--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -1,2 +1,3 @@
 class WishlistItem < ActiveRecord::Base
+  validates :title, presence: true
 end

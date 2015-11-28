97fabd056623c93c5459c8c8a995d992acacefae
bDino <buskulic.dino@gmail.com>
2015-11-28 16:59:05 +0100

    added some functionality to the wishlist model

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,5 +1,8 @@
 class Wishlist < ActiveRecord::Base
-
   validates :title, presence: true
-  validates :description, presence: true
+  has_many :wishlist_items
+
+  def getAllItems
+    return wishlist_items
+  end
 end

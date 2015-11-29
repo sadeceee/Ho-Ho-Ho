ac8094fb6fd174284d0471fc6b241304440177ae
Dorothee Laugwitz <dorothee.laugwitz@gmail.com>
2015-11-29 12:01:37 +0100

    Set range fpr valid withslist item prices

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -1,4 +1,5 @@
 class WishlistItem < ActiveRecord::Base
   validates :title, presence: true
+  validates :price, presence: true, numericality: true, inclusion: { in: 0..1000000, message: "Ungültiger Preis" }
   belongs_to :wishlist
 end

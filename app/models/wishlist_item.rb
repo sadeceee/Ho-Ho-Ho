8793f8b030bec508f9a2144624686d1aa44b15dd
Michael Hanisch <Michael.hanisch@haw-hamburg.de>
2015-11-29 14:27:17 +0100

    added name to checked check-box

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -3,5 +3,6 @@ class WishlistItem < ActiveRecord::Base
   validates :price, presence: true, numericality: true, inclusion: { in: 0..1000000, message: "Ungültiger Preis" }
   validates :priority, presence: true, numericality: true, inclusion: { in: 1..1000 }
   belongs_to :wishlist
+  belongs_to :checked_by, class_name: "User"
   default_scope { order(:priority) }
 end
